f41dd78f0b1277b179f7349b7c844ed5a6566c7c
Timo Wilhelm <mail@timowilhelm.com>
2023-10-09 17:28:48 +0200

    update router

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,52 @@
 import type { Env } from './env';
 
 export { YjsProvider } from './durable/YjsProvider';
-import { Router, error } from 'itty-router';
+import { Router, error, IRequest } from 'itty-router';
 import index from './index.html';
 
+type CF = [env: Env, context: ExecutionContext];
+
+type RoomIdRequest = {
+  roomid: string;
+} & IRequest;
+
+const router = Router();
+
+router
+  .get(
+    '/',
+    () =>
+      new Response(index, {
+        headers: { 'Content-Type': 'text/html' },
+      }),
+  )
+  .get<RoomIdRequest, CF>('/yjs/:roomid', (request, env) => {
+    const { roomid } = request;
+
+    const id = env.DURABLE_YJSPROVIDER.idFromName(roomid);
+    const obj = env.DURABLE_YJSPROVIDER.get(id);
+
+    const doUrl = new URL('https://example.com/connect');
+
+    doUrl.searchParams.set('username', String(request.cf?.country) ?? 'Anonymous'); //TODO: Here you could get the username from the request session
+
+    const req = new Request(doUrl, request);
+
+    return obj.fetch(req);
+  })
+  .get<RoomIdRequest, CF>('/yjs/:roomid/snapshot', (request, env) => {
+    const { roomid } = request;
+
+    const id = env.DURABLE_YJSPROVIDER.idFromName(roomid);
+    const obj = env.DURABLE_YJSPROVIDER.get(id);
+
+    const doUrl = new URL('https://example.com/snapshot');
+    const req = new Request(doUrl, request);
+    return obj.fetch(req);
+  })
+
+  .all('*', () => error(404));
+
 export default {
-  async fetch(request: Request, env: Env) {
-    const router = Router();
-
-    router
-      .get(
-        '/',
-        () =>
-          new Response(index, {
-            headers: { 'Content-Type': 'text/html' },
-          }),
-      )
-      .get('/yjs/:roomid', ({ roomid }) => {
-        const id = env.DURABLE_YJSPROVIDER.idFromName(roomid);
-        const obj = env.DURABLE_YJSPROVIDER.get(id);
-
-        const doUrl = new URL('https://example.com/connect');
-
-        doUrl.searchParams.set('username', String(request.cf?.country) ?? 'Anonymous'); //TODO: Here you could get the username from the request session
-
-        const req = new Request(doUrl, request);
-
-        return obj.fetch(req);
-      })
-      .get('/yjs/:roomid/snapshot', ({ roomid }) => {
-        const id = env.DURABLE_YJSPROVIDER.idFromName(roomid);
-        const obj = env.DURABLE_YJSPROVIDER.get(id);
-
-        const doUrl = new URL('https://example.com/snapshot');
-        const req = new Request(doUrl, request);
-        return obj.fetch(req);
-      })
-
-      .all('*', () => error(404));
-
-    const url = new URL(request.url);
-
-    return router.handle(request, url.pathname);
-  },
+  fetch: (req, ...args) => router.handle(req, ...args).catch(error),
 } satisfies ExportedHandler<Env>;
