563e0580fb30cc2afcacb7aeae58dcea84ad52e0
Timo Wilhelm <mail@timowilhelm.com>
2025-05-29 20:10:05 +0200

    load document snapshot before connecting

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,32 @@ import { studio } from '@outerbase/browsable-durable-object';
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/yjs/:roomId', (c) => {
+app.get('/yjs/snapshot/:roomId', async (c) => {
 	const roomId = c.req.param('roomId');
 
 	const id = c.env.DURABLE_YJSPROVIDER.idFromName(roomId);
-	const obj = c.env.DURABLE_YJSPROVIDER.get(id);
+	const stub = c.env.DURABLE_YJSPROVIDER.get(id);
+
+	const snapshot = await stub.getSnapshot();
+	return new Response(snapshot, {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/octet-stream',
+			'Cache-Control': 'no-store', // Prevent caching of the snapshot
+		},
+	});
+});
+
+app.get('/yjs/ws/:roomId', (c) => {
+	const roomId = c.req.param('roomId');
+
+	const id = c.env.DURABLE_YJSPROVIDER.idFromName(roomId);
+	const stub = c.env.DURABLE_YJSPROVIDER.get(id);
 
 	const doUrl = new URL('https://example.com/ws');
 
 	const req = new Request(doUrl, { headers: c.req.raw.headers });
-	return obj.fetch(req);
+	return stub.fetch(req);
 });
 
 if (import.meta.env.DEV) {
